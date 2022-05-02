2674c28c145ce65e4b4cc2afe545880e7e02597a
subash1706 <subash.somasundaram@chainsys.com>
2022-05-02 09:55:39 +0530

    first commit
    Signed-off-by: subash1706 <subash.somasundaram@chainsys.com>

--- project-eleven/src/app/app.module.ts
+++ project-eleven/src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule ,ReactiveFormsModule} from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EmpformComponent } from './empform/empform.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmpformComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
